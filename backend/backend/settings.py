5e4a6d0b0485f846e7157a2efd1ce51eb4291f97
dhrunam <djb.sibin@gmail.com>
2025-11-28 12:51:41 +0530

    docker files are added...

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/static/'
+
+STATICFILES_DIRS = []
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
